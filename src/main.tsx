5831cf71a8e36fe15fd90ae24a3f325c58b28669
nino <dankozero111@gmail.com>
2024-02-22 00:55:09 +0300

    [add]: import styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import '@app/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
