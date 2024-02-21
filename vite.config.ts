1dfa019417bc7d4ee8cb0e7529c5bceb096a929e
nino <dankozero111@gmail.com>
2024-02-22 00:39:55 +0300

    [build]:update depend-s && allias

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@widgets': path.resolve(__dirname, 'src/widgets'),
       '@shared': path.resolve(__dirname, 'src/shared'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
     },
   },
 });
