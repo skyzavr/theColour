91afe0686295423c7541e21f01b3493a8d321727
nino <dankozero111@gmail.com>
2024-02-22 00:51:44 +0300

    [add]: button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from 'react';
+
+import classes from './button.module.css';
+
+type props = {
+  text: string;
+  onClickFunc: () => void;
+  type: string;
+  children?: ReactElement;
+};
+
+const Button = ({ text, onClickFunc, type = 'none', children }: props) => {
+  return (
+    <button onClick={onClickFunc} className={`${classes.btn} ${classes[type]}`}>
+      {text}
+      <div className={classes.icon}>{children}</div>
+    </button>
+  );
+};
+export default Button;
