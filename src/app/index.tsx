18c448100fb28a7b810d22b875189a0ce58b6188
nino <dankozero111@gmail.com>
2024-02-24 01:00:07 +0300

    [update]: update router path

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ import ThemeContextProvider from './context/ThemeContext';
 import { Home } from '@pages/home';
 import { Layout } from '@widgets/layout';
 import { NotFound } from '@pages/notFound';
+import { Contrast } from '@pages/contrastChecker';
 
 const App = () => {
   const initTheme = localStorage.getItem('theme') || 'dark';
@@ -13,6 +14,7 @@ const App = () => {
       element: <Layout />,
       children: [
         { path: '/', element: <Home /> },
+        { path: '/contrast', element: <Contrast /> },
         {
           path: '*',
           element: <NotFound />,
