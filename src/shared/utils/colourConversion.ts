e63b2c559f2c2c14d51aab83133e8e794c6baa2b
nino <dankozero111@gmail.com>
2024-03-04 23:31:13 +0300

    [feat]: add rgbToHex convertion

--- src/shared/utils/colourConversion.ts
+++ src/shared/utils/colourConversion.ts
@@ -1,6 +1,11 @@
+export const colourString = (colour: string) => {
+  return colour.startsWith('#')
+    ? colour.slice(1).toLowerCase()
+    : colour.toLowerCase();
+};
 export const hexToDec = (num: string) => {
   const letter = 'abcdef';
-  num = num.startsWith('#') ? num.slice(1).toLowerCase() : num.toLowerCase();
+  num = colourString(num);
   const numLen = num.length - 1;
 
   const accum = (acc: number, item: string, ind: number) => {
@@ -9,12 +14,11 @@ export const hexToDec = (num: string) => {
       : Number(item);
     return acc + currentValue * Math.pow(16, numLen - ind);
   };
-
   return [...num].reduce(accum, 0);
 };
 
 export const hexToRGB = (colour: string) => {
-  const list = colour.match(/..?/g);
+  const list = colourString(colour).match(/..?/g);
   if (list === null) return [0, 0, 0];
   return list.map((el) => hexToDec(el));
 };
@@ -69,3 +73,24 @@ export const HexToHSL = (colour: string) => {
   const s = delta / (1 - Math.abs(2 * l - 1));
   return [Math.floor(h), updParam(s), updParam(l)];
 };
+export const decimalToHex = (value: number) => {
+  if (value >= 255) return 'FF';
+  if (value === 0) return '00';
+  const alphbet = 'ABCDEF';
+  let result = '';
+  while (value >= 16) {
+    const maxCount = Math.floor(value / 16);
+    if (maxCount > 9) result += alphbet[maxCount - 10];
+    else result += maxCount;
+    value = value - 16 * maxCount;
+  }
+  if (value > 9) result += alphbet[value - 10];
+  else result += value;
+  if (result.length === 1) result = '0' + result;
+  return result;
+};
+export const rgbToHex = (colour: string) =>
+  colour
+    .split(',')
+    .map((el) => decimalToHex(Number(el)))
+    .join('');
